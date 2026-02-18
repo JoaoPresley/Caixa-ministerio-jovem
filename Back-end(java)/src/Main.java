d3dc2ec153e0fecc00f843f619b97af0cfca0223
JoaoPresley <jvalencarmg@gmail.com>
2026-02-18 16:44:57 -0400

    Criação do menu inicial

--- Back-end(java)/src/Main.java
+++ Back-end(java)/src/Main.java
@@ -12,4 +12,32 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+    public static int menu_inicial(){
+        Scanner sc = new Scanner(System.in);
+        int op = -1;
+        var opcoes = List.of(1, 2, 3, 4, 10);
+
+        do{
+            System.out.println("Escolha uma opção:");
+            System.out.print("""
+                    ----------------------------------------
+                    1 - Cadastrar produto
+                    2 - Cadastrar cliente
+                    3 - Acessar produto
+                    4 - Acessar cliente
+                    10 - SAIR                
+                    ---------------------------------------
+                    Digite uma das opções: """);
+            op = sc.nextInt();
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            if (!opcoes.contains(op)) {
+                System.out.println("!!!!!! Opção inválida! Tente novamente !!!!!!!!!!");
+            }
+
+        }while (!opcoes.contains(op));
+        return op;
+    }
 }
