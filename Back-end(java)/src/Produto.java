0f681ad8480cc50f0cd0b1797c2bead20063ce6c
JoaoPresley <jvalencarmg@gmail.com>
2026-02-18 16:48:06 -0400

    Criado metodo statico cadastrar

--- Back-end(java)/src/Produto.java
+++ Back-end(java)/src/Produto.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Produto {
     String nome;
     double valor; //Valor de venda
@@ -12,8 +15,27 @@ public class Produto {
         this.valor = valor;
         this.quantidade = quantidade;
     }
-    public Produto(String nome){
-        this(nome, 0, 0);
+
+    public static Produto cadastrar(ArrayList <Produto> lista){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o nome do produto: ");
+        String nome = sc.nextLine();
+        //valida se o produto já não foi cadastrado
+        for (Produto p: lista){
+            if(p.get_nome().equalsIgnoreCase(nome)){
+                System.out.println("!!!Produto já cadastrado!!!");
+                return null;
+            }
+        }
+
+        System.out.print("Digite o valor do produto (0 caso não tenha): ");
+        double valor = sc.nextDouble();
+        System.out.print("Digite a quantidade do produto (0 caso não tenha): ");
+        int quantidade = sc.nextInt();
+
+        Produto p = new Produto(nome, valor, quantidade);
+        return p;
     }
 
     //getters
