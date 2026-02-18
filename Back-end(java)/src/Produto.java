109816ec7fef0a24d5bcf5e270e2189904157141
JoaoPresley <jvalencarmg@gmail.com>
2026-02-18 12:40:38 -0400

    Criado getters

--- Back-end(java)/src/Produto.java
+++ Back-end(java)/src/Produto.java
@@ -3,6 +3,7 @@ public class Produto {
     double valor; //Valor de venda
     int quantidade;
 
+    //Construtor
     public Produto(String nome, double valor, int quantidade){
         if (valor < 0) throw new IllegalArgumentException("Preço inválido");
         if (quantidade < 0) throw  new IllegalArgumentException("Quantidade Invalida");
@@ -13,5 +14,16 @@ public class Produto {
     }
     public Produto(String nome){
         this(nome, 0, 0);
+    }
+
+    //getters
+    public String get_nome(){
+        return this.nome;
+    }
+    public double get_valor(){
+        return  this.valor;
+    }
+    public int get_quatidade(){
+        return  this.quantidade;
     }
 }
