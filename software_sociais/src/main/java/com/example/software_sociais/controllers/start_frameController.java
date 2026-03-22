3c373f95897b40d7fb2e00312bd848536b56bba8
JoaoPresley <jvalencarmg@gmail.com>
2026-03-22 18:17:46 -0400

    Inserida interações de iniciar vendas -> opções com e sem evento

--- software_sociais/src/main/java/com/example/software_sociais/controllers/start_frameController.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/start_frameController.java
@@ -1,5 +1,37 @@
 package com.example.software_sociais.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class start_frameController {
+
+    @FXML
+    private VBox vboxMenuPrincipal;
+
+    @FXML
+    private VBox vboxMenuVendas;
+
+    @FXML
+    void clicouIniciarVendas(ActionEvent event) {
+        // Esconde o principal
+        vboxMenuPrincipal.setVisible(false);
+        vboxMenuPrincipal.setManaged(false);
+
+        // Mostra o de vendas
+        vboxMenuVendas.setVisible(true);
+        vboxMenuVendas.setManaged(true);
+    }
+
+    @FXML
+    void clicouVoltar(ActionEvent event) {
+        // Esconde o principal
+        vboxMenuPrincipal.setVisible(true);
+        vboxMenuPrincipal.setManaged(true);
+
+        // Mostra o de vendas
+        vboxMenuVendas.setVisible(false);
+        vboxMenuVendas.setManaged(false);
+    }
 
 }
