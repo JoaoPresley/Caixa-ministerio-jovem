4f82740f0526b971388ed630f598f0d403711326
JoaoPresley <jvalencarmg@gmail.com>
2026-03-22 18:27:01 -0400

    Alterado método de criação de tabelas vendas e eventos, ativada foreignkey

--- software_sociais/src/main/java/com/example/software_sociais/database/Connection_database.java
+++ software_sociais/src/main/java/com/example/software_sociais/database/Connection_database.java
@@ -11,32 +11,36 @@ public class Connection_database {
             if(conection == null || conection.isClosed() ) {
                 String url = "jdbc:sqlite:database.db";
                 conection = DriverManager.getConnection(url);
+                //Ativa as chaves estrangeiras
+                conection.createStatement().execute("PRAGMA foreignkey = ON");
+
                 //Cria tabelas
-                create_tables();
+                createTables();
             }
         } catch (SQLException e) {
             System.err.println("Erro ao conectar com banco de dados: "+e.getMessage());
         }
         return conection;
     }
-    private static void create_tables(){
+    private static void createTables(){
         //Criando tabela vendas
-        String sql = "CREATE TABLE IF NOT EXISTS Eventos(\n" +
+        String tbEventos_sql = "CREATE TABLE IF NOT EXISTS Eventos(\n" +
                 "\tid INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                 "\tnome TEXT,\n" +
                 "\tdata TEXT DEFAULT (date('now'))\n" +
-                ");\n" +
-                "\n" +
-                "CREATE TABLE IF NOT EXISTS Vendas(\n" +
+                ");";
+
+        String tbVendas_sql = "CREATE TABLE IF NOT EXISTS Vendas(\n" +
                 "\tid INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                 "\tid_evento INTEGER,\n" +
                 "\tproduto TEXT,\n" +
-                "\tpreco REAL NOT NULL,\n" +
+                "\tpreco NUMERIC NOT NULL,\n" +
                 "\t--Criação da chave estrangeira\n" +
-                "\tFOREIGN KEY (id_evento) REFERENCES Eventos(id)\n" +
+                "\tFOREIGN KEY (id_evento) REFERENCES Eventos(id) ON DELETE CASCADE\n" +
                 ");";
         try (Statement stmt = conection.createStatement()){
-            stmt.execute(sql);
+            stmt.execute(tbEventos_sql);
+            stmt.execute(tbVendas_sql);
         }catch (SQLException e){
             e.printStackTrace();
         }
