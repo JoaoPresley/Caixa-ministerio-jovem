df988436ba0a8a06653c925bfe6e5cb91494f0d5
JoaoPresley <jvalencarmg@gmail.com>
2026-03-26 20:32:02 -0400

    Adicionado funcao clickCriarEvento

--- software_sociais/src/main/java/com/example/software_sociais/controllers/start_frameController.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/start_frameController.java
@@ -1,8 +1,15 @@
 package com.example.software_sociais.controllers;
 
+import com.example.software_sociais.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class start_frameController {
 
@@ -11,6 +18,20 @@ public class start_frameController {
 
     @FXML
     private VBox vboxMenuVendas;
+
+    @FXML
+    void clickCriarEvento(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/software_sociais/create_evento.fxml"));
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            Scene scene = new Scene(fxmlLoader.load());
+            window.setScene(scene);
+            window.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     @FXML
     void clicouIniciarVendas(ActionEvent event) {
