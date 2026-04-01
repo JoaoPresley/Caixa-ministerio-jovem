b04a0b9b4a61afdf995e41b4d219142f04ea7ff2
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 17:23:47 -0400

    Estruturado view_vendas.fxml

--- software_sociais/src/main/java/com/example/software_sociais/controllers/view_vendasController.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/view_vendasController.java
@@ -1,4 +1,36 @@
 package com.example.software_sociais.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class view_vendasController {
+
+    @FXML
+    private TableView<?> tabelaVenda;
+
+    @FXML
+    private Label tituloVenda;
+
+    @FXML
+    private TableColumn<?, ?> vendaData;
+
+    @FXML
+    private TableColumn<?, ?> vendaProduto;
+
+    @FXML
+    private TableColumn<?, ?> vendaValor;
+
+    @FXML
+    void clickIniciar_vendas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickVoltar(ActionEvent event) {
+
+    }
+
 }
