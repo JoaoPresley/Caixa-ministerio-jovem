649f6721cd6737dab178b4cb04ba1e14d5aff782
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 11:14:53 -0400

    debugado acesso aos objetos

--- software_sociais/src/main/java/module-info.java
+++ software_sociais/src/main/java/module-info.java
@@ -7,6 +7,9 @@ module com.example.software_sociais {
 
     opens com.example.software_sociais to javafx.fxml;
     exports com.example.software_sociais;
-    exports com.example.software_sociais.controllers;
     opens com.example.software_sociais.controllers to javafx.fxml;
+    exports com.example.software_sociais.controllers;
+
+    opens com.example.software_sociais.objects to javafx.fxml, javafx.base;
+    exports com.example.software_sociais.objects;
 }
