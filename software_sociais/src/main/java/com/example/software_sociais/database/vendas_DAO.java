39afbc8d791a4f7e848a1e7e3c0d8412039164c8
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 17:40:20 -0400

    Criado metodo Insert no vendas_DAO

--- software_sociais/src/main/java/com/example/software_sociais/database/vendas_DAO.java
+++ software_sociais/src/main/java/com/example/software_sociais/database/vendas_DAO.java
@@ -2,12 +2,28 @@ package com.example.software_sociais.database;
 
 import com.example.software_sociais.objects.Venda;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class vendas_DAO implements DAO <Venda> {
     //Inserir dados na tabela vendas
     @Override
     public void insert(Venda venda){
+        String sql = "INSERT INTO Vendas (id_evento, produto, valor)\n" +
+                "VALUES (?, ?, ?)";
+        try (Connection conn = Connection_database.conect()){
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, venda.getId_evento());
+            ps.setString(2, venda.getProduto());
+            ps.setDouble(3, venda.getValor());
+
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -22,6 +38,7 @@ public class vendas_DAO implements DAO <Venda> {
 
     @Override
     public List<Venda> listar() {
-        return List.of();
+        List <Venda> lista = new ArrayList<>();
+        return lista;
     }
 }
