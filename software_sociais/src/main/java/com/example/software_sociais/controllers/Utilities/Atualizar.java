f05a516a734c967e8cc1cef8351547c9e641fce1
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 09:59:16 -0400

    Criado atualizações - tabela

--- software_sociais/src/main/java/com/example/software_sociais/controllers/Utilities/Atualizar.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/Utilities/Atualizar.java
@@ -1,4 +1,14 @@
 package com.example.software_sociais.controllers.Utilities;
 
-public class AtualizarTabela {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+
+import java.util.List;
+
+public class Atualizar <T>{
+    public void AtualizarTabela(TableView<T> tabela, List<T> lista){
+        ObservableList<T> obslista = FXCollections.observableArrayList(lista);
+        tabela.setItems(obslista);
+    }
 }
