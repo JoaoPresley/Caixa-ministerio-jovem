cfc9fa43a5a4db3f10e85de0d81b804890d68624
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 17:39:36 -0400

    Adicionado interface initiablre no view_vendasContorller

--- software_sociais/src/main/java/com/example/software_sociais/controllers/view_vendasController.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/view_vendasController.java
@@ -1,27 +1,37 @@
 package com.example.software_sociais.controllers;
 
+import com.example.software_sociais.objects.Venda;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class view_vendasController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class view_vendasController implements Initializable {
 
     @FXML
-    private TableView<?> tabelaVenda;
+    private TableView<Venda> tabelaVenda;
 
     @FXML
     private Label tituloVenda;
 
     @FXML
-    private TableColumn<?, ?> vendaData;
+    private TableColumn<Venda, String> vendaData;
 
     @FXML
-    private TableColumn<?, ?> vendaProduto;
+    private TableColumn<Venda, String> vendaProduto;
 
     @FXML
-    private TableColumn<?, ?> vendaValor;
+    private TableColumn<Venda, Double> vendaValor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     @FXML
     void clickIniciar_vendas(ActionEvent event) {
