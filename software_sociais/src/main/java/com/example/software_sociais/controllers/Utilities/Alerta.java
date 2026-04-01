44588b2490defb5ecfca8602c55c9e3f575002eb
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 09:58:27 -0400

    Criado alerta

--- software_sociais/src/main/java/com/example/software_sociais/controllers/Utilities/Alerta.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/Utilities/Alerta.java
@@ -1,4 +1,13 @@
 package com.example.software_sociais.controllers.Utilities;
 
+import javafx.scene.control.Alert;
+
 public class Alerta {
+    public static void alerta(String titulo, String texto, Alert.AlertType tipo) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(texto);
+        alert.showAndWait();
+    }
 }
