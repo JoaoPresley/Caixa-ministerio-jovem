ba725412fa6447020dab435a3b92285d59af7c09
JoaoPresley <jvalencarmg@gmail.com>
2026-04-01 20:32:14 -0400

    Preenchimento do init_vendas.fxml

--- software_sociais/src/main/java/com/example/software_sociais/controllers/init_vendasController.java
+++ software_sociais/src/main/java/com/example/software_sociais/controllers/init_vendasController.java
@@ -1,4 +1,40 @@
 package com.example.software_sociais.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class init_vendasController {
+
+    @FXML
+    private TableColumn<?, ?> colProduto;
+
+    @FXML
+    private TableColumn<?, ?> colValor;
+
+    @FXML
+    private Label lbl_titulo;
+
+    @FXML
+    private TableView<?> tabelaVendas;
+
+    @FXML
+    private TextField txt_vendaProduto;
+
+    @FXML
+    private TextField txt_vendaValor;
+
+    @FXML
+    void clickRealizarVenda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickVoltar(ActionEvent event) {
+
+    }
+
 }
